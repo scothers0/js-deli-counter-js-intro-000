4553395c400dbb2ba32ec61cc6a57c3abbe09201
harmonious-cache-6099 <isaac_scothern@hotmail.co.uk>
2020-03-12 18:31:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length > 0){
-    return `The line is currently: ${0}. ${katzDeliLine[0].toString()},`;
+    return `The line is currently: ${0 + 1}. ${katzDeliLine[0].toString()},`;
   }
   return "The line is currently empty.";
 }
