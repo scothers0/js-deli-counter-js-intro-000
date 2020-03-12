78326cb06f9e3bea7ad1ec33e26c22db12f14911
harmonious-cache-6099 <isaac_scothern@hotmail.co.uk>
2020-03-12 18:50:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,10 +13,16 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  if (katzDeliLine.length > 0){
-    for (var i = 0; i < katzDeliLine.length; i++){
+  if (katzDeliLine.length > 1){
+    let printOut = `The line is currently: `
+    for (var i = 0; i < katzDeliLine.length - 1; i++){
+
+      printOut.concat(`${i + 1}. ${katzDeliLine[i].toString()}, `)
     return `The line is currently: ${i + 1}. ${katzDeliLine[i].toString()},`;
     }
+  }
+  else if (katzDeliLine.length = 1) {
+    return `The line is currently: 1. ${katzDeliLine[0].toString()}`;
   }
   return "The line is currently empty.";
 }
