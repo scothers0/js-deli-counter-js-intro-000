fde2fb541d7d8ce144c91484468858ce1a4711e1
harmonious-cache-6099 <isaac_scothern@hotmail.co.uk>
2020-03-12 18:02:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0){
     return `Currently serving ${katzDeliLine[0]}.`;
     katzDeliLine.shift();
+    return katzDeliLine;
   }
   return "There is nobody waiting to be served!";
 
